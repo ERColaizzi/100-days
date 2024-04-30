59aa37962644f91b62265087ce030b6c06a80906
ercolaizzi <ercolaizzi@gmail.com>
2024-04-29 23:12:02 -0400

    init

---
+++ Day5.py
@@ -0,0 +1,38 @@
+#looping
+#
+#password generator
+#input length, numbers, special
+# string lenth less inputs
+#generate rand capitals 1-third/half
+#rand insert numb,special
+#
+
+import random
+import string
+
+
+inp_len = int(input("How long of a pssword would you like?: "))
+num_num = int(input ("How many numbers would you like?: "))
+num_chars = int(input("How many special characters would you like?: "))
+
+length = inp_len - num_chars - num_num
+
+
+##string.ascii_letters
+#acii.lower + ascii.upper
+##string.ascii_digits
+##string.punctuation
+
+
+while True:
+    
+
+
+
+
+
+
+
+
+
+
